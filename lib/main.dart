2db380fb6ca0fed300e04975a77402b9e77dd65f
MohamedAliBadawy <medooali6262@gmail.com>
2024-05-10 00:33:48 +0300

    pull from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:amazon/controller/provider/auth_provider.dart';
+import 'package:amazon/controller/services/auth_bloc/auth_bloc.dart';
+import 'package:amazon/controller/services/login_bloc/login_bloc.dart';
 import 'package:amazon/utils/theme.dart';
-import 'package:amazon/view/Home/component/user_bottom_nav_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
@@ -11,24 +13,54 @@ Future main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const Amazon());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(),
+      ),
+      BlocProvider<LoginBloc>(
+        create: (context) => LoginBloc(),
+      ),
+    ],
+    child: const Amazon(),
+  ));
 }
 
-class Amazon extends StatelessWidget {
+class Amazon extends StatefulWidget {
   const Amazon({super.key});
 
   @override
+  State<Amazon> createState() => _AmazonState();
+}
+
+class _AmazonState extends State<Amazon> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<AuthBloc>(context).add(AppStartEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<Authprovider>(create: (_) => Authprovider()),
-      ],
-      child: MaterialApp(
-        theme: theme,
-        // home: const AuthScreen(),
-        home: const UserBottomNavBar(),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      theme: theme,
+      home: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is Authenticated) {
+            return const HomeScreen();
+          } else if (state is Unauthenticated) {
+            return const AuthScreen();
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        },
       ),
+      // home: UserBottomNavBar(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
