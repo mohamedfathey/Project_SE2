9070952274e3c660c37f0be49dfdc49fdfb0c6f4
Omar Abdullah <omarmoharam790@gmail.com>
2024-05-15 23:58:26 +0300

    to fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:amazon/controller/blocs/auth_bloc/auth_bloc.dart';
 import 'package:amazon/controller/blocs/login_bloc/login_bloc.dart';
 import 'package:amazon/controller/provider/Product_Provider.dart';
-import 'package:amazon/controller/provider/auth_provider.dart';
 import 'package:amazon/controller/provider/deal_of_the_provider.dart';
 import 'package:amazon/controller/provider/product_by_category_provider.dart';
 import 'package:amazon/controller/provider/rating_provider.dart';
 import 'package:amazon/controller/provider/users_product_provider.dart';
-
 import 'package:amazon/utils/theme.dart';
 import 'package:amazon/view/Home/component/user_bottom_nav_bar.dart';
 import 'package:amazon/view/auth_screen.dart';
-import 'package:amazon/view/home_screen.dart';
 import 'package:amazon/view/seller/seller_persistant_nav_bar/seller_bottom_nav_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
